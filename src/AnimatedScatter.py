93a90ca718d9caa9c6ac6fc9198a7c2403c1a538
Vemund <vemund@live.com>
2021-03-18 19:22:22 +0100

    Adds loss tracking to animation, and saves animation in html format

--- src/AnimatedScatter.py
+++ src/AnimatedScatter.py
@@ -1,7 +1,9 @@
 """
-Code initially taken from (17/03-2021):
+Code starting point taken from (17/03-2021):
 https://stackoverflow.com/questions/9401658/how-to-animate-a-scatter-plot
 """
+import matplotlib
+matplotlib.use('nbAgg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
@@ -10,7 +12,7 @@ import numpy as np
 class AnimatedScatter(object):
     """An animated scatter plot using matplotlib.animations.FuncAnimation."""
 
-    def __init__(self, cluster_data, weight_data):
+    def __init__(self, cluster_data, weight_data, loss_history):
         """
         Args:
             cluster_data: (N,2), where N=4*n -> four clusters
@@ -18,17 +20,19 @@ class AnimatedScatter(object):
         """
         self.cluster_data = cluster_data
         self.weight_data = weight_data
+        self.loss_history = loss_history
         # Setup the figure and axes...
         self.fig, self.ax = plt.subplots()
         # Then setup FuncAnimation.
+        self.save_count=weight_data.shape[0]
         self.animation = animation.FuncAnimation(
             self.fig,
             self.update,
             init_func=self.setup_plot,
             interval=25, # time in ms between frames
-            repeat_delay=1000, # delay before loop
-            blit=False, # for OSX
-            save_count=weight_data.shape[0], # #frames
+            #repeat_delay=1000, # delay before loop
+            blit=False, # for OSX?
+            save_count=self.save_count, # #frames
         )
 
     def setup_plot(self):
@@ -36,12 +40,13 @@ class AnimatedScatter(object):
         init_weights = self.weight_data
         data = self.cluster_data
         colors = ["red", "green", "blue", "orange"]
-        means = np.mean(
+        self.means = np.mean(
             np.reshape(data, (4, int(data.shape[0] / 4), data.shape[-1])), axis=1
         )
+        color_idxs = np.argmax(self.means @ init_weights[0],axis=0) # shapes (4,2) @ (2,4)
         cluster_N = int(data.shape[0] / 4)
         self.weight_arrows = []
-        for color, mean, i in zip(colors, means, range(len(colors))):
+        for color, mean, i in zip(colors, self.means, range(len(colors))):
             self.ax.scatter(
                 data[i * cluster_N : (i + 1) * cluster_N, 0],
                 data[i * cluster_N : (i + 1) * cluster_N, 1],
@@ -55,19 +60,23 @@ class AnimatedScatter(object):
                 width=0.01,
                 color=(0, 0, 0, 0.5),  # semi-transparent black arrow
             )
+            
             warrow = self.ax.arrow(
                 0,
                 0,
                 *init_weights[0, :, i],
                 length_includes_head=True,
                 width=0.01,
-                color=(1, 0, 0, 0.5),  # semi-transparent green arrow
+                color=colors[color_idxs[i]],#rscolor=(1, 0, 0, 0.5),  # semi-transparent green arrow
             )
             self.weight_arrows.append(warrow)
         self.ax.grid("on")
+        self.ax.set_title('Loss={}'.format(self.loss_history[0]))
 
     def update(self, k):
         """Update the scatter plot."""
+        colors = ["red", "green", "blue", "orange"]
+        color_idxs = np.argmax(self.means @ self.weight_data[k],axis=0) # shapes (4,2) @ (2,4)
         for i in range(4):
             self.weight_arrows.pop(0).remove()  # delete arrow
             warrow = self.ax.arrow(
@@ -76,9 +85,11 @@ class AnimatedScatter(object):
                 *self.weight_data[k, :, i],
                 length_includes_head=True,
                 width=0.01,
-                color=(1, 0, 0, 0.5),
+                color=colors[color_idxs[i]],#color=(1, 0, 0, 0.5),
             )
             self.weight_arrows.append(warrow)
+
+        self.ax.set_title('Loss={}'.format(self.loss_history[k]))
 
         # We need to return the updated artist for FuncAnimation to draw..
         # Note that it expects a sequence of artists, thus the trailing comma.
--- src/InspectWeights.py
+++ src/InspectWeights.py
@@ -5,9 +5,11 @@ class InspectWeights(tf.keras.callbacks.Callback):
     def __init__(self, **kwargs):
         super(InspectWeights, self).__init__(**kwargs)
         self.weight_history = []
+        self.loss_history = [0]
 
     def on_train_begin(self, logs=None):
         self.weight_history.append(self.model.get_weights())
 
     def on_epoch_end(self, epoch, logs=None):
         self.weight_history.append(self.model.get_weights())
+        self.loss_history.append(logs['loss'])
